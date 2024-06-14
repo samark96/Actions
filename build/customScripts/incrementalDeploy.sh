8c660c6b2ba856358b12a5f774a5afed76a18921
Samar Koganti <samar.koganti@mass.gov>
2024-06-14 15:53:08 -0400

    c

--- build/customScripts/incrementalDeploy.sh
+++ build/customScripts/incrementalDeploy.sh
@@ -2,6 +2,8 @@
 git config --global --add safe.directory /__w/Actions/Actions
 git config --global user.email "GitHub@mass.gov"
 git config --global user.name "GitHub User"
+sfdx --version
+sfdx plugins --core
 latestCommitId=$(git log -n 1 --pretty=format:"%h" | tail -n 1 2>&1)
 echo ""
 echo "latest commit id is $latestCommitId"
