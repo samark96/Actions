74efce6dcca76911cfdd94c7a9aeeebe6c301fbc
Samar Koganti <samar.koganti@mass.gov>
2024-06-14 10:23:35 -0400

    c

--- build/customScripts/incrementalDeploy.sh
+++ build/customScripts/incrementalDeploy.sh
@@ -5,4 +5,19 @@ echo ""
 echo "latest commit id is $latestCommitId"
 echo ""
 echo "Last successful commit id is $fromCommitId"
+echo ""
+sfdx sgd:source:delta --to "$latestCommitId" --from "$fromCommitId" --output "."
+echo ""
+echo ""
+echo "--- package.xml generated with added and modified metadata ---"
+echo "--- Validate changes in the following package.xml thats deployed ---"
+echo ""
+echo ""
+cat package/package.xml
+echo ""
+echo ""
+echo ""
+echo ""
+mkdir ./changed-sources
+sfdx sgd:source:delta --to "$latestCommit" --from "$fromCommit" --output changed-sources/ --generate-delta
 echo "fromCommitId=${latestCommitId}" >> $GITHUB_ENV
