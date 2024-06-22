4ab4e066028d564314b183b804ac222ef69c9f18
Samar Koganti <samar.koganti@mass.gov>
2024-06-22 19:54:29 -0400

    c

--- build/customScripts/updateVariable.ps1
+++ build/customScripts/updateVariable.ps1
@@ -2,14 +2,10 @@ param(
     [string]$commitId
 )
 Write-Host "latest commit id is $commitId"
-Write-Host ""
-#
-#
-# /repos/{owner}/{repo}/environments/{environment_name}/variables/{name}
-#gh api \
-#  --method PATCH \
-#  -H "Accept: application/vnd.github+json" \
-#  -H "Authorization: Bearer $personalToken" \
-#  -H "X-GitHub-Api-Version: 2022-11-28" \
-#  /repos/samark96/Actions/environments/prod/variables/fromCommitId \
-#  -d '{"name":"fromCommitId","value":"'$fromCommitId'"}'
+
+gh api /repos/samark96/Actions/environments/prod/variables/fromCommitId \
+  --method PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $personalToken" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -d '{"name":"fromCommitId","value":"'$commitId'"}'
