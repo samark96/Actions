8bde4b41047cf3fe709a499ed27913dbd569333f
Samar Koganti <samar.koganti@mass.gov>
2024-06-22 19:03:32 -0400

    c

--- build/customScripts/deltaDeploy.ps1
+++ build/customScripts/deltaDeploy.ps1
@@ -2,10 +2,10 @@ git config --global --add safe.directory /__w/Actions/Actions
 git config --global user.email "GitHub@mass.gov"
 git config --global user.name "GitHub User"
 
-$latestCommit = git log -1 --pretty=oneline
-$latestCommitId = $latestCommit.Substring(0, $latestCommit.IndexOf(' '))
+#$latestCommit = git log -1 --pretty=oneline
+$latestCommitId = git log -n 1 --pretty=format:"%h" #$latestCommit.Substring(0, $latestCommit.IndexOf(' '))
 Write-Host " "
 Write-Host "Latest Commit Id is : $latestCommitId"
 Write-Host ""
-Write-Host "Last successful commit id is $fromCommitId"
+Write-Host "Last successful commit id is $Env:fromCommitId"
 Write-Host "fromCommitId=${latestCommitId}" >> $GITHUB_ENV
