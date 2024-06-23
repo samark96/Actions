83f1dfbe6d4dcc2df172a1979332ff4740b43f9d
Samar Koganti <samar.koganti@mass.gov>
2024-06-23 13:31:32 -0400

    c

--- build/customScripts/getChanges.ps1
+++ build/customScripts/getChanges.ps1
@@ -1,6 +1,5 @@
 
-#$latestCommit = git log -1 --pretty=oneline
-$latestCommitId = git log -n 1 --pretty=format:"%h" #$latestCommit.Substring(0, $latestCommit.IndexOf(' '))
+$latestCommitId = git log -n 1 --pretty=format:"%h"
 Write-Host " "
 Write-Host "Latest Commit Id is : $latestCommitId"
 Write-Host ""
